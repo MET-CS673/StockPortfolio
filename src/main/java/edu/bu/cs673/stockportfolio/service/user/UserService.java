2dec70b63406f003732b69f4ce07dda80c9b6b19
Michael Lewis <mjolewis@gmail.com>
2021-06-16 19:10:36 -0400

    FEAT: Added stubs for Services

---
+++ src/main/java/edu/bu/cs673/stockportfolio/service/user/UserService.java
@@ -0,0 +1,13 @@
+package edu.bu.cs673.stockportfolio.service.user;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**********************************************************************************************************************
+ * Implements business logic for User requests.
+ *
+ *********************************************************************************************************************/
+@Service
+@Transactional
+public class UserService {
+}
