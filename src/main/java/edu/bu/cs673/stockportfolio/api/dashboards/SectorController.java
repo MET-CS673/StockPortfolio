256d7c73799bebc70407d932a4f3e6dc6c20d4be
ccelia365 <44828051+ccelia365@users.noreply.github.com>
2021-07-21 18:04:42 -0400

    Sector Controller Comments
    Added Javadoc comments

--- src/main/java/edu/bu/cs673/stockportfolio/api/dashboards/SectorController.java
+++ src/main/java/edu/bu/cs673/stockportfolio/api/dashboards/SectorController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+/**
+ * 
+ * The controller for Sector Breakdown.
+ */
 @Controller
 @RequestMapping("/sector_breakdown")
 public class SectorController {
 
+    /**
+     * Provides a view of the sector breakdown
+     * 
+     * @return  sector breakdown of a user's portfolio
+     */
     @GetMapping
     public String sectorBreakdownView() {
         return "sector_breakdown";
