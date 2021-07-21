59766a42643071a47d3f22a7ec3cb6eb70ef17e7
ccelia365 <44828051+ccelia365@users.noreply.github.com>
2021-07-21 18:18:09 -0400

    Security Controller Comments
    Added Javadoc comments

--- src/main/java/edu/bu/cs673/stockportfolio/api/dashboards/SecurityController.java
+++ src/main/java/edu/bu/cs673/stockportfolio/api/dashboards/SecurityController.java
@@ -21,6 +21,13 @@ import edu.bu.cs673.stockportfolio.service.portfolio.PortfolioService;
 import edu.bu.cs673.stockportfolio.service.user.UserService;
 import edu.bu.cs673.stockportfolio.service.utilities.ResponseService;
 
+/**
+ * 
+ * The Controller for Stock Breakdown.
+ * 
+ * Depends on PortfolioService, ResponseService and UserService.
+ * 
+ */
 @Controller
 @RequestMapping("/stock_breakdown")
 public class SecurityController {
@@ -28,6 +35,13 @@ public class SecurityController {
     private final ResponseService responseService;
     private final UserService userService;
 
+    /**
+     * Class constructor that handles the view of the stock breakdown
+     * 
+     * @param portfolioService  the portfolio imported by the user
+     * @param responseService   packages the responses from portfolioService
+     * @param userService       ensures that the correct authenticated user is making the request
+     */
     public SecurityController(PortfolioService portfolioService, ResponseService responseService, UserService userService) {
 
         this.portfolioService = portfolioService;
@@ -35,6 +49,13 @@ public class SecurityController {
         this.userService = userService;
     }
 
+    /**
+     * Provides a view of the stock breakdown
+     * 
+     * @param authentication    checks if the user is authenticated
+     * @param model             contains the data from portfolioService and responseService
+     * @return                  the stock breakdown viewed by user
+     */
     @GetMapping
     public String securityBreakdownView(Authentication authentication, Model model) {
 
@@ -54,6 +75,12 @@ public class SecurityController {
         return "stock_breakdown";
     }
 
+    /**
+     * Checks if the correct user is authenticated and has access to their individual account
+     * 
+     * @param authentication    checks the credentials of the user
+     * @return                  True if the authenticated user is registered
+     */
     private User getUser(Authentication authentication) {
 
         return userService.findUserByName(authentication.getName());
