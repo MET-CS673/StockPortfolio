b11c564d342dd2cf54cd5435f82fe885a5177a50
ccelia365 <44828051+ccelia365@users.noreply.github.com>
2021-07-21 17:48:17 -0400

    AccountController Comments
    Added Javadoc comments

--- src/main/java/edu/bu/cs673/stockportfolio/api/account/AccountController.java
+++ src/main/java/edu/bu/cs673/stockportfolio/api/account/AccountController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+/**
+ * 
+ * The controller for a user's account
+ */
 @Controller
 @RequestMapping("/account")
 public class AccountController {
 
+    /**
+     * Allows a user to view their account
+     * 
+     * @return  a user's account
+     */
     @GetMapping
     public String accountView() {
         return "account";
