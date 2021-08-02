2b8f2f84031124c854d323769315a4d772b9a0fe
Cecilia <cecilc365@gmail.com>
2021-08-02 00:58:17 -0400

    api-SignupController-comments
    Javadoc

--- src/main/java/edu/bu/cs673/stockportfolio/api/signup/SignupController.java
+++ src/main/java/edu/bu/cs673/stockportfolio/api/signup/SignupController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**********************************************************************************************************************
- * This controller handles signup requests. It depends on the UserService to check if the username provided already
- * exists. If not, delegates account creation to the UserService.
+ * This controller handles signup requests. It depends on the UserService to check
+ * if the username provided already exists. If not, delegates account creation 
+ * to the UserService.
  *
+ * Controller maps to the url request "/signup"
  *********************************************************************************************************************/
 @Controller
 @RequestMapping("/signup")
@@ -21,15 +23,36 @@ public class SignupController {
 
     private final UserService userService;
 
+    /**
+    *   Class constructor that initializes the dependency to UserService
+    *
+    *   @param userService The User request object
+    */
     public SignupController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+    *   When HTTP GET request is recieved for /signup, signupView()
+    *   is called to handle the request
+    *
+    *   @return "signup" when /signup is requested
+    */
     @GetMapping()
     public String signupView() {
         return "signup";
     }
 
+    /**
+    *   Maps HTTP POST request onto specifc handler method signup.
+    *   Checks to see if the username provided already exists and redirects to login
+    *   upon successful signup
+    *
+    *   @param user Creates a new address object for user and puts it into model
+    *   @param model Takes the result of signing up and adds it to model.
+    *   @param redirectAttributes Stores flash attributes on successful signup
+    *   @return "signup" when /signup is requested
+    */
     @PostMapping
     public String signup(@ModelAttribute User user, Model model, RedirectAttributes redirectAttributes) {
         String signupError = null;
