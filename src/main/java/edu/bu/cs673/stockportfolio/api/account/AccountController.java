d4f410469ead2772e3389be939d78732ab36b179
Cecilia <cecilc365@gmail.com>
2021-08-01 23:37:19 -0400

    api-AccountController-comments
    Javadoc

--- src/main/java/edu/bu/cs673/stockportfolio/api/account/AccountController.java
+++ src/main/java/edu/bu/cs673/stockportfolio/api/account/AccountController.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+* Handles the request to view "/account"
+* Controller maps to the url request "/account"
+* 
+*/
 @Controller
 @RequestMapping("/account")
 public class AccountController {
 
+    /**
+    *   When HTTP GET request is received for /account, accountView()
+    *   is called to handle the request
+    *
+    *   @return "account" when /account is requested
+    */
     @GetMapping
     public String accountView() {
         return "account";
