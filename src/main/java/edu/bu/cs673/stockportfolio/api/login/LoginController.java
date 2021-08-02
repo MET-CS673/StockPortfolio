d646adc0acd50f861c5f3df2e9e6c0735a732f7d
Cecilia <cecilc365@gmail.com>
2021-08-02 00:29:39 -0400

    api-LoginController-comments
    Javadoc

--- src/main/java/edu/bu/cs673/stockportfolio/api/login/LoginController.java
+++ src/main/java/edu/bu/cs673/stockportfolio/api/login/LoginController.java
@@ -5,12 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**********************************************************************************************************************
- * Spring automatically leverages the AuthenticationService class to verify a users identity.
+ * Spring automatically leverages the AuthenticationService class to verify a users
+ * identity.
+ *
+ *Controller maps to the url request "/login"
  *********************************************************************************************************************/
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
+    /**
+    *   When HTTP GET request is received for /login, loginView()
+    *   is called to handle the request
+    *
+    *   @return "login" when /login is requested
+    */
     @GetMapping
     public String loginView() {
         return "login";
