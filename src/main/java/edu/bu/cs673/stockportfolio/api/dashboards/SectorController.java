72fba57503ff585e906801fe3a843823a06ab078
ccelia365 <44828051+ccelia365@users.noreply.github.com>
2021-08-05 08:51:12 -0400

    api-SectorBreakdown-comments
    Javadoc

--- src/main/java/edu/bu/cs673/stockportfolio/api/dashboards/SectorController.java
+++ src/main/java/edu/bu/cs673/stockportfolio/api/dashboards/SectorController.java
@@ -18,6 +18,9 @@ import edu.bu.cs673.stockportfolio.service.portfolio.PortfolioService;
 import edu.bu.cs673.stockportfolio.service.user.UserService;
 import edu.bu.cs673.stockportfolio.service.utilities.ResponseService;
 
+/**
+ * Controller bean responsible for handling "sector_breakdown" requests
+ */
 @Controller
 @RequestMapping("/sector_breakdown")
 public class SectorController {
@@ -26,6 +29,14 @@ public class SectorController {
     private final ResponseService responseService;
     private final UserService userService;
 
+    /**
+     * Creates an SectorController. (Autowired by Spring)
+     * Responsible for handling request(s) to show sector breakdowns.
+     * 
+     * @param portfolioService An autowired implementation of the PortfolioService (provided by the Spring dependency injection)
+     * @param responseService An autowired implementation of the ResponseService (provided by the Spring dependency injection)
+     * @param userService An autowired implementation of the UserService (provided by the Spring dependency injection)
+     */
     public SectorController(PortfolioService portfolioService, ResponseService responseService, UserService userService) {
 
         this.portfolioService = portfolioService;
@@ -33,6 +44,19 @@ public class SectorController {
         this.userService = userService;
     }
 
+    /**
+     * GET Mapping for '/sector_breakdown', shows the "sector_breakdown.html" view.
+     * 
+     * This method will take the authenticated user from Spring Authentication result and
+     * look up the user's portfolio using portfolioService. From the portfolio, it will
+     * get the user's accounts and then it will aggregate the account data by sector and 
+     * add that result to the model attribute "data" for rendering in the 
+     * view '/sector_breakdown.html'.
+     * 
+     * @param authentication The Spring authentication object - used to get the User principal
+     * @param model Model object to provide data to template
+     * @return The name of the view to show 'resources/templates/sector_breakdown.html'
+     */
     @GetMapping
     public String sectorBreakdownView(Authentication authentication, Model model) {
         
@@ -52,6 +76,12 @@ public class SectorController {
         return "sector_breakdown";
     }
 
+    /**
+     * Get the User object from the Authentication principal.
+     * 
+     * @param authentication The Spring authentication object - used to get the User principal
+     * @return The User object if found, null otherwise
+     */
     private User getUser(Authentication authentication) {
 
         return userService.findUserByName(authentication.getName());
