7db4a1a46956e85acfa62db511091129076018c5
ccelia365 <44828051+ccelia365@users.noreply.github.com>
2021-08-05 09:23:53 -0400

    api-UserProfileController-comments
    Javadoc

--- src/main/java/edu/bu/cs673/stockportfolio/api/userprofile/UserProfileController.java
+++ src/main/java/edu/bu/cs673/stockportfolio/api/userprofile/UserProfileController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Controller bean responsible for handling "profile" requests
+ */
 @Controller
 @RequestMapping("/profile")
 public class UserProfileController {
@@ -25,17 +28,49 @@ public class UserProfileController {
     private final HashService hashService;
     private final FluentLogger log = FluentLoggerFactory.getLogger(PortfolioService.class);
 
+    /**
+     * Creates a UserProfileController. (Autowired by Spring)
+     * Responsible for handling request to show the profile page.
+     * 
+     * @param userService An autowired implementation of the UserService (provided by the Spring dependency injection)
+     * @param responseService An autowired implementation of the ResponseService (provided by the Spring dependency injection)
+     * @param hashService An autowired implementation of the HashService (provided by the Spring dependency injection)
+     */
     public UserProfileController(UserService userService, ResponseService responseService, HashService hashService) {
         this.userService = userService;
         this.responseService = responseService;
         this.hashService = hashService;
     }
 
+    /**
+     * GET Request Handler for '/profile', shows the "user_profile.html" view.
+     * 
+     * @return The user_profile view ... (resources/templates/user_profile.html)
+     */
     @GetMapping()
     public String getUserProfile() {
         return "user_profile";
     }
 
+    /**
+     * POST Request Handler for '/profile/delete'
+     * 
+     * Attempts to delete a user's profile.
+     * 
+     * This method will take the authenticated user from the Spring Authentication result
+     * and delete the user using the user service. After deleting the user, will clear the 
+     * jsession cookie and show the "signup.html" view.
+     * 
+     * If the user does not exist, or the user service raises an exception, the handler will
+     * show the deletePortfolioError(...) view ('results.html').
+     * 
+     * An error will be logged if an exception is thrown.
+     * 
+     * @param authentication The Spring authentication object - used to get the User principal
+     * @param response The http servlet response object
+     * @param model Model object to provide data to template
+     * @return The view
+     */
     @PostMapping("/delete")
     public String deleteUserProfile(Authentication authentication, HttpServletResponse response, Model model) {
         User currentUser = getCurrentUser(authentication);
@@ -61,6 +96,20 @@ public class UserProfileController {
         return responseService.deletePortfolioError(true, model);
     }
 
+    /**
+     * POST Request Handler for '/profile/modifyPwd'
+     * 
+     * This method will take the authenticated user from Spring Authentication result and 
+     * uses the UserService to get the current user. If there is a current user, it will
+     * first verify the old password, and if verified, will try to change the password
+     * to the new password. If this succeeds, then it will return an html response with
+     * "true" as the response body. Otherwise it will return "false" in the response body.
+     * 
+     * @param authentication The Spring authentication object - used to get the current user
+     * @param oldPassword The old password
+     * @param newPassword The new password
+     * @return The response body either "true" or "false"
+     */
     @ResponseBody
     @PostMapping("/modifyPwd")
     public String modifyPassword(Authentication authentication,
@@ -85,6 +134,12 @@ public class UserProfileController {
         return "false";  // The oldPassword does not match the password stored in our database
     }
 
+    /**
+     * Get the current User, if it exists, from the Authentication principal.
+     * 
+     * @param authentication The Spring authentication object - used to get the User principal
+     * @return The current User object if found, null otherwise
+     */
     private User getCurrentUser(Authentication authentication) {
         return userService.findUserByName(authentication.getName());
     }
