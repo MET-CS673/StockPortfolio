a855ec7c2783ba7fab5eaf90a0f71815f11941c3
ccelia365 <44828051+ccelia365@users.noreply.github.com>
2021-08-05 22:46:23 -0400

    api-HomeController-comments
    Javadoc

--- src/main/java/edu/bu/cs673/stockportfolio/api/login/HomeController.java
+++ src/main/java/edu/bu/cs673/stockportfolio/api/login/HomeController.java
@@ -21,6 +21,8 @@ import java.util.List;
 
 /**********************************************************************************************************************
  * Handles requests to populate the homepage after a user is logged in.
+ * 
+ * Controller bean responsible for "home" requests
  *********************************************************************************************************************/
 @Controller
 @RequestMapping("/home")
@@ -31,6 +33,14 @@ public class HomeController {
     private final ResponseService responseService;
     private final FluentLogger log = FluentLoggerFactory.getLogger(HomeController.class);
 
+    /**
+     * Creates a HomeController. (Autowired by Spring)
+     * Responsible for handling request(s) to show the homepage.
+     * 
+     * @param userService An autowired implementation of the UserService (provided by the Spring dependency injection)
+     * @param portfolioService An autowired implementation of the PortfolioService (provided by the Spring dependency injection)
+     * @param responseService An autowired implementation of the ResponseService (provided by the Spring dependency injection)
+     */
     public HomeController(UserService userService, PortfolioService portfolioService,
                           ResponseService responseService) {
         this.userService = userService;
@@ -38,6 +48,31 @@ public class HomeController {
         this.responseService = responseService;
     }
 
+    /**
+     * <h3>GET Mapping '/home', shows the "home.html" view</h3>
+     * 
+     * <p>This method will take the authenticated user from Spring Authentication result,
+     * and add it to the {@code model}.</p>
+     * 
+     * <p>If the user's portfolio property is not null, it will try to fetch the portfolio
+     * from PortfolioService using the id of the portfolio. If the portfolio exists, it will get 
+     * the list of accounts and create a portfolio table using
+     * {@link edu.bu.cs673.stockportfolio.service.utilities.ResponseService#createPortfolioTable(List)
+     * responseService.createPortfolioTable(...)}. Then this is added to model attribute "portfolio"
+     * for rendering in the view '/home.html'.</p>
+     * 
+     * <p>If the portfolio is not found, then add an empty ArrayList to model attribute "portfolio"
+     * for rendering in the view '/home.html'.</p>
+     * 
+     * <p>If the portfolio is null(user does not have a portfolio), add an empty ArrayList to 
+     * model attribute "portfolio" for rendering in the view '/home.html'.</p>
+     * 
+     * @param authentication The Spring authentication object - used to get the User principal
+     * @param model Model object to provide data to template
+     * @return The name of the view to show 'resources/templates/home.html'
+     * @see edu.bu.cs673.stockportfolio.service.utilities.ResponseService#createPortfolioTable(List)
+     * responseService.createPortfolioTable(...)
+     */
     @GetMapping
     public String getHomePage(Authentication authentication, Model model) {
         User user = getUser(authentication);
@@ -62,6 +97,12 @@ public class HomeController {
         return "home";
     }
 
+    /**
+     * Get the User object from the Authentication principal.
+     * 
+     * @param authentication The Spring authentication object - used to get the User principal
+     * @return The User object if found, null otherwise
+     */
     private User getUser(Authentication authentication) {
         return userService.findUserByName(authentication.getName());
     }
